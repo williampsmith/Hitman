7191a5879aac0d20deccaf5647279dad56c6e683
Gera Groshev <groshevg@berkeley.edu>
2017-11-18 23:41:40 -0800

    evade

--- common.py
+++ common.py
@@ -197,7 +197,7 @@ class PacketUtils:
         #     ack=synack_pkt[IP][TCP].seq + 1,
         #     sport=send_port,
         # )
-        print(synack_pkt)
+        # print(synack_pkt)
         seq_offset = 1
         chunk_size = 10
         letters = "abcdefghijklmnopqrstuvwxyz"
