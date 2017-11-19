5ed0cf0ca94b4b9573227a5c4fe2bc7c8cd8404e
Gera Groshev <groshevg@berkeley.edu>
2017-11-19 00:21:17 -0800

    evade

--- common.py
+++ common.py
@@ -207,26 +207,40 @@ class PacketUtils:
             msg = msg[chunk_size:]
             rand_msg = ''.join([random.choice(letters) for _ in range(chunk_size)])
 
-            # if len(msg) == 0:
-            # if random.randint(0, 1)
+            if random.randint(0, 1) == 0:
+                pkt = self.send_pkt(
+                    payload=payload,
+                    flags="PA",
+                    seq=send_seq + seq_offset,
+                    ack=synack_pkt[IP][TCP].seq + 1,
+                    sport=send_port,
+                )
 
-            pkt = self.send_pkt(
-                payload=payload,
-                flags="PA",
-                seq=send_seq + seq_offset,
-                ack=synack_pkt[IP][TCP].seq + 1,
-                sport=send_port,
-            )
+                pkt = self.send_pkt(
+                    payload=rand_msg,
+                    ttl=ttl,
+                    flags="PA",
+                    seq=send_seq + seq_offset,
+                    ack=synack_pkt[IP][TCP].seq + 1,
+                    sport=send_port,
+                )
+            else:
+                pkt = self.send_pkt(
+                    payload=rand_msg,
+                    ttl=ttl,
+                    flags="PA",
+                    seq=send_seq + seq_offset,
+                    ack=synack_pkt[IP][TCP].seq + 1,
+                    sport=send_port,
+                )  
 
-
-            pkt = self.send_pkt(
-                payload=rand_msg,
-                ttl=ttl,
-                flags="PA",
-                seq=send_seq + seq_offset,
-                ack=synack_pkt[IP][TCP].seq + 1,
-                sport=send_port,
-            )
+                pkt = self.send_pkt(
+                    payload=payload,
+                    flags="PA",
+                    seq=send_seq + seq_offset,
+                    ack=synack_pkt[IP][TCP].seq + 1,
+                    sport=send_port,
+                )
 
             seq_offset += len(payload)
 
