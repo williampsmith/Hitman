84eb4f0fa87e7f6cd4e0f49d82dd46b88755e41b
Gera Groshev <groshevg@berkeley.edu>
2017-11-21 15:04:11 -0800

    evade test

--- common.py
+++ common.py
@@ -198,8 +198,7 @@ class PacketUtils:
             msg = msg[chunk_size:]
             rand_msg = ''.join([random.choice(letters) for _ in range(chunk_size)])
 
-            # Make sure the terminating new line characters get delivered
-            if len(msg) + len(payload) > 4:
+            if len(msg) + len(payload) > 5:
                 if random.randint(0, 1) == 0:
                     pkt = self.send_pkt(
                         payload=payload,
@@ -235,6 +234,7 @@ class PacketUtils:
                         sport=send_port,
                     )
             else:
+                # Make sure the terminating new line characters get delivered
                 print(msg)
                 print(payload)
                 pkt = self.send_pkt(
